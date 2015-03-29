b00b994ebb22f73b8483e4788ddc6a2eea7923dc
waveUbuntu <hiiwave@gmail.com>
2015-03-29 20:31:11 +0800

    ok

--- clientjs.js
+++ clientjs.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
 	var prependNewMsg = function(packet) {
-		var newTr = '<tr>  \
-		      <td>' + packet._sender + '</td>  \
-		      <td>' + packet._msg + '</td>     \
-		      <td>' + packet._time + '</td>    \
-		    </tr>';
+		var newTr = '<tr> \
+			<td>' + packet._sender + '</td> \
+			<td>' + packet._msg + '</td> \
+			<td><span class="faceicon faceicon-sm ' + packet._face + '"></span></td> \
+			<td>' + packet._time + '</td> \
+			</tr>';
 		$("#board tbody").prepend(newTr);
 	};
 
@@ -17,7 +18,7 @@ $(document).ready(function() {
 		xhr.onload = sendSuccess;
 		xhr.send(data);	
 	};
-
+       
 	var retrieveDataFromServer = function() {
 		console.log("Retrieve!");
 		var xhr = new XMLHttpRequest();
@@ -27,16 +28,22 @@ $(document).ready(function() {
 			$("#board tbody").html("");
 			var pktAry = JSON.parse(this.responseText);
 			for (var i = 0, len = pktAry.length; i < len; i += 1) {
-				var aPacket = pktAry[i];
-				$("#maxMsgIdField").val(packet._msgId);
+				var aPacket = JSON.parse(pktAry[i]);
+				// console.log("aPacket = " + aPacket);
 				prependNewMsg(aPacket);
-
 			}
 		};
 		xhr.send();
 	};
 	$("#refresh").click(retrieveDataFromServer);
 
+	var getFaceSelect = function() {
+	var face = $("#face_dropdown-toggle .faceicon").attr('class').replace("faceicon faceicon-sm ", "");
+		return face;
+	}
+
+	//retrieveDataFromServer();
+	
 	var sendMessage = function() {
 		var sender = $("#msgSender").val();
 		var msg = $("#msgBody").val();
@@ -45,15 +52,19 @@ $(document).ready(function() {
 		}
 		$("#msgBody").val("");
 		var time = "...";  // TODO: get current time
-		console.log(sender + " is going to send \"" + msg + "\"");
+		var face = getFaceSelect(); // TODO: pack in packet
+		var faceToogleBtn = $("#face_dropdown-toggle .faceicon");
+		faceToogleBtn.attr('class', "faceicon faceicon-sm smile");
+		console.log(sender + " is going to send \"" + msg + "\" with face: " + face);
 		var packet = {
 			_sender : sender,
 			_msg : msg,
+			_face : face,
 			_time : time,
 		}
 		prependNewMsg(packet);
 		postDataToServer(JSON.stringify(packet));
-	}
+	};
 	$("#btnSend").click(sendMessage);
 	$(document).keypress(function (e) {
     	if ($("#msgBody").is(":focus") && (e.which == 13)) {
@@ -70,7 +81,7 @@ $(document).ready(function() {
 			// TODO: sort by time 
 		}
 		$("#sortMethod").val(method);
-	}
+	};
 	var checkBeforeSort = function(method) {
 		if ($("#sortMethod").val() == method) {
 			console.log("Has been sorted!")
@@ -83,6 +94,18 @@ $(document).ready(function() {
 	});
 	$("#sort_time").click(function() {
 		checkBeforeSort("time");
+	});
+
+	// change faceicon
+	$("#face_dropdown li a").click(function() {
+		// console.log("Click: " + $(this).eq(0).html());
+		var newClass = $(this).children().eq(0).attr('class');
+		newClass = newClass.replace("faceicon ", '');
+		console.log("Will append newClass: " + newClass);
+		var toogleBtn = $("#face_dropdown-toggle .faceicon");
+		toogleBtn.attr('class', "faceicon faceicon-sm");
+		toogleBtn.addClass(newClass);
+		console.log("Select Face: " + getFaceSelect()) ;
 	});
 });
 	
